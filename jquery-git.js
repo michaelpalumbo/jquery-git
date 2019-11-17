6405883fd8e1394e5d38993bce90c7bf1b4145c2
Michael Palumbo <michaelpalumbo@users.noreply.github.com>
2019-11-17 12:51:26 -0500

    update for gitshow

--- jquery-git.js
+++ jquery-git.js
@@ -33,6 +33,8 @@
 
         // Get the repositories
         $.getJSON(uri, function(data) {
+
+
             if (data && data.length) {
                 err.remove();
                 var html = '',
@@ -45,8 +47,122 @@
                 }
 
                 $(data).each(function(i, v) {
-                    var date = new Date(v.updated_at);
-                    html += '<a class="jquery-git-repo-link" href="' +
+                    let r = /\d/.test((/.$/).exec(v.name))
+                    let num = (/.$/).exec(v.name)
+                    // filter out non-gitshow repos and the main gitshow admin repo
+                    switch(r){
+                        case true:
+                            
+                            fetch(v.url +'/commits')
+                            .then(
+                                function(response) {
+                                if (response.status !== 200) {
+                                    console.log('Looks like there was a problem. Status Code: ' +
+                                    response.status);
+                                    return;
+                                }
+
+                                // Examine the text in the response
+                                response.json().then(function(data) {
+                                    
+
+                                    for(i = 0; i < data.length; i++){
+                                        // console.log(data[i].commit.author.name, data[i].commit.author.date, data[i].commit.message, data[i].html_url);
+
+                                        html = '<br>Author: ' + data[i].commit.author.name + '<br>Date: ' + data[i].commit.author.date + '<br>Version notes: ' + data[i].commit.message + '<br>  <a class="jquery-git-repo-link" href="' +
+                                        data[i].html_url + '" target="_blank">View Changes</div><br><a href="' + v.url + '/zipball/' + data[i].sha + '" target="_blank"><u><b>Download</u></b><p>'
+                                        var $toProcess = $( html );
+
+
+                                        // FWIW YOU could also make a button to download a 
+                                        // $toProcess.find( "div" ).addClass( "processed" );
+                
+                                        // getting by id before insertion
+                                        // alert( $toProcess.find( "#myDiv1" ).html() );
+                                        // alert( $( "#myDiv1" ).html() ); // this will return null, since the divs 
+                                                                        // were not yet added to the document
+                
+                                        $toProcess.appendTo( "#details" + num );
+                                    }
+                                });
+                                }
+                            )
+                            .catch(function(err) {
+                                console.log('Fetch Error :-S', err);
+                            });
+                            // console.log(v)
+                        
+
+                        $('#gitshow' + num).repo({ user: 'dispersionlab', name: 'gitshow'  + num })
+                            
+                        /*
+                        html = '<a class="jquery-git-repo-link" href="' +
+                        v.html_url + '" target="_blank">' +
+                            (
+                                exclude.indexOf('name') > -1 ?
+                                    '' : '<h3>' + v.name + '</h3>'
+                            ) +
+                            (
+                                exclude.indexOf('updatedAt') > -1 ?
+                                    '' : '<i>Last Updated: ' + [
+                                        date.getMonth() + 1,
+                                        date.getDate(),
+                                        date.getFullYear()
+                                    ].join('/') + ' ' + [
+                                        date.getHours() > 12 ?
+                                            date.getHours() - 12 : date.getHours(),
+                                        date.getMinutes()
+                                    ].join(':') +
+                                    (date.getHours() > 12 ? 'PM' : 'AM') +
+                                    '</i>'
+                            ) +
+                            (
+                                v.description && exclude.indexOf('description') > -1 ?
+                                    '' : '<div>' + v.description + '</div>'
+                            );
+                        $(include).each(function(i, w) {
+                            html += '<span>' + (v[ w ] || '') + '</span>';
+                        });
+                        html += '</a>';
+
+                        // console.log(v.name, update)
+                        */
+
+
+                        // ('#details1').append(html)
+                        break;
+
+
+                        case false:
+
+
+                        break;
+
+
+                    }
+
+                });
+
+                if (dataLengthLimited) {
+                    html += '<a class="jquery-git-owner-link" href="' +
+                    data[ 0 ].owner.html_url + '" target="_blank">' + (
+                        opts.limitText || 'See More Here!'
+                    ) +'</a>';
+                }
+
+                // me.append(html);
+                
+                $("[data-repo]").github();
+            }
+        }).error(function() {
+            console.warn(
+                '$.git: Repositories cannot be fetched while not connected to the internet.'
+            );
+        });
+
+        return this;
+    };
+})();+
                     v.html_url + '" target="_blank">' +
                         (
                             exclude.indexOf('name') > -1 ?
